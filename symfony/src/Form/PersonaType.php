2e028bad6cb207e8110ba6007481f3d1f0cd8df2
gcharasson <gabin.charasson@etu.u-bordeaux.fr>
2023-06-27 14:59:33 +0200

    fix: :bug: fix routes

---
+++ symfony/src/Form/PersonaType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Persona;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('libelle')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Persona::class,
+        ]);
+    }
+}
