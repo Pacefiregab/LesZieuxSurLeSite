842441fa330ed28b537d125490a0aac955500a9d
Grégoire <nodilod@gmail.com>
2023-06-27 11:19:51 +0200

    call api on event

--- symfony/public/js/custom.js
+++ symfony/public/js/custom.js
@@ -1,33 +1,60 @@
+(function ($) {
 
-  (function ($) {
-  
   "use strict";
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
+  // MENU
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $('body,html').animate({
         scrollTop: totalScroll
-        }, 300);
-      }
+      }, 300);
+    }
+  });
+
+  $(document).on('keydown', function (event) {
+    if (event.key == 'i' && event.ctrlKey) {
+      // on scroll down or up event
+      $(document).on('scroll', function (event) {
+        sendTrackingData(event)
+      })
+    }
+
+    $(document).on('click', function (event) {
+      sendTrackingData(event)
     });
-  
-  })(window.jQuery);
+  });
+})(window.jQuery);
+
+function sendTrackingData(event) {
+  $.ajax({
+    url: '127.0.0.1/tracking',
+    type: 'POST',
+  }).json({
+    'flag': $(this).data('flag') || null,
+    'type': 'click',
+    'date': new Date(),
+    'x': event.pageX,
+    'y': event.pageY,
+  }).then(function (response) {
+    console.log(response);
+  });
+}
+
 
 
