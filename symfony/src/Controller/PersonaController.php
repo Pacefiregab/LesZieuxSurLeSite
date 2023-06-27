0c65a52db9149eeec259f161231dc33983f8f931
gcharasson <gabin.charasson@etu.u-bordeaux.fr>
2023-06-27 11:46:13 +0200

    create  crud routes for controllers

---
+++ symfony/src/Controller/PersonaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/persona')]
+class PersonaController extends AbstractController
+{
+    #[Route('/', name: 'index_persona', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('persona/index.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/create', name: 'create_persona', methods: ['GET'])]
+    public function create(): Response
+    {
+        return $this->render('persona/create.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/create', name: 'add_persona', methods: ['POST'])]
+    public function add(): Response
+    {
+        return $this->render('persona/create.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit_persona', methods: ['GET'])]
+    public function edit(): Response
+    {
+        return $this->render('persona/edit.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit_persona', methods: ['POST'])]
+    public function modify(): Response
+    {
+        return $this->render('persona/edit.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete_persona', methods: ['DELETE'])]
+    public function delete(): Response
+    {
+        return $this->render('persona/delete.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'show_persona', methods: ['GET'])]
+    public function show(): Response
+    {
+        return $this->render('persona/show.html.twig', [
+            'controller_name' => 'PersonaController',
+        ]);
+    }
+
+}
