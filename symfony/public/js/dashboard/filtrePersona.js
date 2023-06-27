c2a7edffb7b6064720d47411b7243ebfcc89be89
Anthonydt16 <anthony.douat@etu.u-bordeaux.fr>
2023-06-27 15:51:17 +0200

    js fais

---
+++ symfony/public/js/dashboard/filtrePersona.js
@@ -0,0 +1,18 @@
+export class filtrePersona{
+
+  /**
+   * @var {NodeListOf} personas - tableau des personas
+   */
+  personas = document.querySelectorAll('.persona');
+  /**
+   * Constructeur de la classe filtrePersona
+   *
+   */
+  constructor(){
+
+    this.personas.forEach(persona => {
+
+    });
+
+  }
+}
--- symfony/src/Controller/DashboardController.php
+++ symfony/src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Persona;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,23 @@ use Symfony\Component\HttpFoundation\Response;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         {
-            return $this->render('dashboard/index.html.twig');
+            //récuperer tous les personas
+            $personas = $entityManager->getRepository(Persona::class)->findAll();
+            //si personas et vide on crée un crée un faux objet persona
+            if (empty($personas)) {
+                for ($i = 0; $i < 3; $i++) {
+                    $persona = new Persona();
+                    $persona->setName('Aucun persona'.$i);
+                    $persona->setLibelle('Aucun persona');
+                    $personas[] = $persona;
+                }
+            }
+            return $this->render('dashboard/index.html.twig', [
+                'personas' => $personas,
+            ]);
         }
     }
 }
