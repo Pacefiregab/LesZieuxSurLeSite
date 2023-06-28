61d581a41a6386ccc10295c9bd27fa2fe6ae6201
gcharasson <gabin.charasson@etu.u-bordeaux.fr>
2023-06-28 08:38:18 +0200

    feat: :sparkles: persona/tracking crud

--- symfony/src/Form/PersonaType.php
+++ symfony/src/Form/PersonaType.php
@@ -3,18 +3,27 @@
 namespace App\Form;
 
 use App\Entity\Persona;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PersonaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('libelle')
-        ;
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
+            ->add('libelle', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ],);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
