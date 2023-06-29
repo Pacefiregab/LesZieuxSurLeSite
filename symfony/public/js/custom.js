9f6f726ec583cf4b7a306dd969e30addf1602e9e
Grégoire <nodilod@gmail.com>
2023-06-29 12:10:52 +0200

    persona

--- symfony/public/js/custom.js
+++ symfony/public/js/custom.js
@@ -116,7 +116,7 @@ function sendRecord(ws) {
       scrollRecord: JSON.stringify(scrollRecord),
       windowHeight,
       windowWidth,
-      session_id: 1 //valeur a modifier
+      session_id: $('input[name="session_id"]').val(),
     }
   })
     .then((data) => {
