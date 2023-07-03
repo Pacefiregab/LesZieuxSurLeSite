ed30a7993ec88ed64cb1bae3cd4605cbbf4e1285
Anthonydt16 <anthony.douat@etu.u-bordeaux.fr>
2023-07-03 15:01:45 +0200

    API OKAY manque le charts

--- symfony/public/js/persona_show/personaShow.js
+++ symfony/public/js/persona_show/personaShow.js
@@ -17,17 +17,13 @@ export class personaShow{
         this.data = data;
         console.log(this.data);
         console.log(this.stat_persona);
-        let date_sessions = document.querySelector('#date_sessions');
-        if (date_sessions != null) {
-            date_sessions
-        }
         if (this.data != null) {
-            this.stat_persona.forEach((stat) => {
-                let element = stat.childNodes[1];
-                console.log(element);
-                console.log(this.data)
-                element.innerHTML = element.innerHTML + this.data[element.id];
-            });
+            document.querySelector('#date_session').innerHTML = this.data.sessionDate
+            document.querySelector('#name_persona').innerHTML = document.querySelector('#name_persona').innerHTML + this.data.persona.name;
+            document.querySelector('#name_session').innerHTML = "Gabin le nom stp";
+            document.querySelector('#duree_session').innerHTML = document.querySelector('#duree_session').innerHTML + this.data.sessionTime;
+            document.querySelector('#nom_template').innerHTML = document.querySelector('#nom_template').innerHTML + this.data.template.name;
+            document.querySelector('#nombre_clique').innerHTML = document.querySelector('#nombre_clique').innerHTML + this.data.template.count;
         }
 
     }
