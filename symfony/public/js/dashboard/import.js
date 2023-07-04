48a8614707d60f98df8b93b8c4510d1554e44f83
Anthonydt16 <anthony.douat@etu.u-bordeaux.fr>
2023-07-04 17:31:08 +0200

    debug api fix sessions details

--- symfony/public/js/dashboard/import.js
+++ symfony/public/js/dashboard/import.js
@@ -6,14 +6,17 @@ let filtre2 = new filtre('/personas/api/', (data) => {
     let dashboard2 = new dashboard(data);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('redirection-link');
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); // Empêche le comportement de lien par défaut
+document.onreadystatechange = function () {
+    document.addEventListener('DOMContentLoaded', function() {
+        var link = document.getElementById('redirection-link');
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Empêche le comportement de lien par défaut
 
-        console.log(window.location.href)
-        // Effectue la redirection en utilisant l'URL spécifiée dans l'attribut "href"
-        window.location.href = link.getAttribute('href');
+            console.log(window.location.href)
+            // Effectue la redirection en utilisant l'URL spécifiée dans l'attribut "href"
+            window.location.href = link.getAttribute('href');
+        });
     });
-});
+}
+
 
