7bcca51384cd94506af20d6f7eb48b61ffbe4b93
TDpy <toto.dupuy6558@gmail.com>
2023-07-05 17:15:30 +0200

    change redirect from templates details

--- symfony/public/js/general.js
+++ symfony/public/js/general.js
@@ -1,6 +1,10 @@
 
 function goBack() {
+    if (url.includes("templates") && url.includes("sessions")) {
+        window.location.replace("http://localhost/templates/details");
+    } else {
         window.location.replace("http://localhost/");
+    }
 }
 
 function launchSession() {
